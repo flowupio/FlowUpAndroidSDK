e30c29a25331964d8c6d5bf42ac5cd29cca0db55
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-05-31 16:14:16 +0200

    Create empty project

---
+++ app/src/main/java/com/karumi/kiru/example/MainActivity.java
@@ -0,0 +1,12 @@
+package com.karumi.kiru.example;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main_activity);
+  }
+}
