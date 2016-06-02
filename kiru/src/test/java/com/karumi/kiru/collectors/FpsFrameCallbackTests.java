14a9d7ee0d863f25fbd85b80701ac87dbc6c6ab7
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-06-02 12:25:48 +0200

    Improve the usage of mockito and add two tests to guarantee that the callback is registering the device data for the next frame

--- kiru/src/test/java/com/karumi/kiru/collectors/FpsFrameCallbackTests.java
+++ kiru/src/test/java/com/karumi/kiru/collectors/FpsFrameCallbackTests.java
@@ -1,10 +1,32 @@
+/*
+ * Copyright (C) 2016 Go Karumi S.L.
+ */
+
 package com.karumi.kiru.collectors;
 
 import android.view.Choreographer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-public class FpsFrameCallbackTests {
+@RunWith(MockitoJUnitRunner.class) public class FpsFrameCallbackTests {
 
-  private final FpsFrameCallback fpsFrameCallback = new FpsFrameCallback(mock(Choreographer.class));
+  private static final long ANY_FRAME_TIME = 11;
+
+  private FpsFrameCallback fpsFrameCallback;
+  @Mock private Choreographer choreographer;
+
+  @Before public void setUp() {
+    fpsFrameCallback = new FpsFrameCallback(choreographer);
+  }
+
+  @Test public void shouldPostAnotherCallbackToTheChoreographerAfterTheDoFrameExecution() {
+    fpsFrameCallback.doFrame(ANY_FRAME_TIME);
+
+    verify(choreographer).postFrameCallback(fpsFrameCallback);
+  }
 }
--- kiru/src/test/java/com/karumi/kiru/collectors/FrameTimeCallbackTest.java
+++ kiru/src/test/java/com/karumi/kiru/collectors/FrameTimeCallbackTest.java
@@ -5,15 +5,25 @@
 package com.karumi.kiru.collectors;
 
 import android.view.Choreographer;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-public class FrameTimeCallbackTest {
+@RunWith(MockitoJUnitRunner.class) public class FrameTimeCallbackTest {
 
-  private final FrameTimeCallback frameTimeCallback =
-      new FrameTimeCallback(mock(Choreographer.class));
+  private static final long ANY_FRAME_TIME = 11;
+
+  private FrameTimeCallback frameTimeCallback;
+  @Mock private Choreographer choreographer;
+
+  @Before public void setUp() {
+    frameTimeCallback = new FrameTimeCallback(choreographer);
+  }
 
   @Test public void shouldCalculateTheAverageFrameTime() {
     frameTimeCallback.doFrame(15);
@@ -26,11 +36,17 @@ public class FrameTimeCallbackTest {
   }
 
   @Test public void shouldReturnZeroAsFrameTimeIfTheCallbackHasBeenReset() {
-    frameTimeCallback.doFrame(15);
+    frameTimeCallback.doFrame(ANY_FRAME_TIME);
 
     frameTimeCallback.reset();
 
     long frameTimeNanos = frameTimeCallback.getFrameTimeNanos();
     assertEquals(0, frameTimeNanos);
+  }
+
+  @Test public void shouldPostAnotherCallbackToTheChoreographerAfterTheDoFrameExecution() {
+    frameTimeCallback.doFrame(ANY_FRAME_TIME);
+
+    verify(choreographer).postFrameCallback(frameTimeCallback);
   }
 }
