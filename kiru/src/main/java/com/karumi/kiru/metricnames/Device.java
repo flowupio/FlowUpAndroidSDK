52e02446811d51330d80595031800229b6d94a28
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-06-02 11:41:31 +0200

    Finish Device class implementation

--- kiru/src/main/java/com/karumi/kiru/metricnames/Device.java
+++ kiru/src/main/java/com/karumi/kiru/metricnames/Device.java
@@ -5,6 +5,8 @@
 package com.karumi.kiru.metricnames;
 
 import android.content.Context;
+import android.os.Build;
+import android.util.DisplayMetrics;
 
 class Device {
 
@@ -15,19 +17,37 @@ class Device {
   }
 
   String getOSVersion() {
-    return "";
+    return String.valueOf(Build.VERSION.SDK_INT);
   }
 
   String getModel() {
-    return "";
+    return Build.MODEL;
   }
 
   String getScreenDensity() {
-    return "";
+    float density = context.getResources().getDisplayMetrics().density;
+    if (density >= 4.0) {
+      return "xxxhdpi";
+    }
+    if (density >= 3.0) {
+      return "xxhdpi";
+    }
+    if (density >= 2.0) {
+      return "xhdpi";
+    }
+    if (density >= 1.5) {
+      return "hdpi";
+    }
+    if (density >= 1.0) {
+      return "mdpi";
+    }
+    return "ldpi";
   }
 
   String getScreenSize() {
-    return "";
+    DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+    int width = displayMetrics.widthPixels;
+    int height = displayMetrics.heightPixels;
+    return width + "X" + height;
   }
-
 }
