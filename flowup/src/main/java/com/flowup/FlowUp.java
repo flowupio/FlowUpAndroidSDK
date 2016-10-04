f1128c628034f85cb10078e8dc4d24f25a2d26ea
Pedro Vicente Gómez Sánchez <pedrovgs@users.noreply.github.com>
2016-10-04 18:57:25 +0200

    Connect with our grafana installation. (#24)
    * Add a new reporter using statsd and hosted in AWS servers
    
    * Add a log reporter
    
    * Rename method as suggested by Davide

--- flowup/src/main/java/com/flowup/FlowUp.java
+++ flowup/src/main/java/com/flowup/FlowUp.java
@@ -7,13 +7,14 @@ package com.flowup;
 import android.app.Application;
 import android.os.Handler;
 import android.os.Looper;
+import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricFilter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
 import com.flowup.collectors.Collector;
 import com.flowup.collectors.Collectors;
-
+import com.readytalk.metrics.StatsDReporter;
 import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
 
@@ -57,15 +58,33 @@ public class FlowUp {
 
   private void initializeMetrics() {
     registry = new MetricRegistry();
-    //ConsoleReporter reporter = ConsoleReporter.forRegistry(registry).build();
+    initializeConsoleReporter();
+    initializeHostedGraphiteReporter();
+    initializeKarumiStatsDReporter();
+  }
+
+  private void initializeConsoleReporter() {
+    ConsoleReporter.forRegistry(registry).build().start(10, TimeUnit.SECONDS);
+  }
+
+  private void initializeKarumiStatsDReporter() {
+    StatsDReporter.forRegistry(registry)
+        .convertRatesTo(TimeUnit.SECONDS)
+        .convertDurationsTo(TimeUnit.MILLISECONDS)
+        .filter(MetricFilter.ALL)
+        .build("54.194.240.147", 8125)
+        .start(10, TimeUnit.SECONDS);
+  }
+
+  private void initializeHostedGraphiteReporter() {
     Graphite graphite = new Graphite(new InetSocketAddress("carbon.hostedgraphite.com", 2003));
-    GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
+    GraphiteReporter.forRegistry(registry)
         .prefixedWith("6f9a168a-ea09-4fdd-8d11-b4c2c36f14e0")
         .convertRatesTo(TimeUnit.SECONDS)
         .convertDurationsTo(TimeUnit.MILLISECONDS)
         .filter(MetricFilter.ALL)
-        .build(graphite);
-    reporter.start(10, TimeUnit.SECONDS);
+        .build(graphite)
+        .start(10, TimeUnit.SECONDS);
   }
 
   private void initializeForegroundCollectors() {
