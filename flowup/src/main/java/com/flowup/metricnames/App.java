14dbcd4135348854ed119bbefc5f0f03dfdd01e3
Pedro Vicente Gómez Sánchez <pedrovgs@users.noreply.github.com>
2016-10-04 18:39:43 +0200

    Change App class implementation to use the package manager instead of the BuildConfig.VERSION_CODE constant (#26)

--- flowup/src/main/java/com/flowup/metricnames/App.java
+++ flowup/src/main/java/com/flowup/metricnames/App.java
@@ -5,7 +5,9 @@
 package com.flowup.metricnames;
 
 import android.content.Context;
-import android.support.graphics.drawable.BuildConfig;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import com.flowup.R;
 
 class App {
 
@@ -20,7 +22,13 @@ class App {
   }
 
   String getApplicationVersion() {
-    return String.valueOf(BuildConfig.VERSION_CODE);
-  }
 
+    try {
+      String packageName = getApplicationName();
+      PackageInfo packageInfo = context.getPackageManager().getPackageInfo(packageName, 0);
+      return packageInfo.versionName.replace(".", "");
+    } catch (PackageManager.NameNotFoundException e) {
+      return context.getString(R.string.unknown_version_name_cross_metric_name);
+    }
+  }
 }
