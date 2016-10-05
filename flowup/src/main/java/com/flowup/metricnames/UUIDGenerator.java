e4a97a8a095b7e70517a5834f5894cd6fd612f1b
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-10-05 18:07:35 +0200

    Add test coverage

--- flowup/src/main/java/com/flowup/metricnames/UUIDGenerator.java
+++ flowup/src/main/java/com/flowup/metricnames/UUIDGenerator.java
@@ -1,23 +1,25 @@
+/*
+ * Copyright (C) 2016 Go Karumi S.L.
+ */
+
 package com.flowup.metricnames;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
 import java.util.UUID;
 
 class UUIDGenerator {
 
   private static final String UUID_KEY = "uuid";
   private static final String UUID_SHARED_PREFS_NAME = "uuid_shared_prefs_name";
+  private static final Object generatorLock = new Object();
 
   private final SharedPreferences sharedPreferences;
 
   UUIDGenerator(Context context) {
     this.sharedPreferences =
         context.getSharedPreferences(UUID_SHARED_PREFS_NAME, Context.MODE_PRIVATE);
-    generateAndSaveUUID();
   }
-
 
   String getUUID() {
     String uuid = getUUIDFromSharedPreferences();
@@ -28,8 +30,11 @@ class UUIDGenerator {
   }
 
   private String generateAndSaveUUID() {
-    String uuid = UUID.randomUUID().toString();
-    saveUUID(uuid);
+    String uuid = "";
+    synchronized (generatorLock) {
+      uuid = UUID.randomUUID().toString();
+      saveUUID(uuid);
+    }
     return uuid;
   }
 
@@ -37,7 +42,7 @@ class UUIDGenerator {
     sharedPreferences.edit().putString(UUID_KEY, uuid).commit();
   }
 
-  @NonNull private String getUUIDFromSharedPreferences() {
+  private String getUUIDFromSharedPreferences() {
     return sharedPreferences.getString(UUID_KEY, "");
   }
 }
