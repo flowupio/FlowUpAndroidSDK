c0249a431aa8ca0dba66f667dc5c9d50f9a5af48
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-10-05 11:16:52 +0200

    Try to get the version name from the build config if there is an exception getting it from the package manager

--- flowup/src/main/java/com/flowup/metricnames/App.java
+++ flowup/src/main/java/com/flowup/metricnames/App.java
@@ -7,6 +7,7 @@ package com.flowup.metricnames;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import com.flowup.BuildConfig;
 import com.flowup.R;
 
 import static com.flowup.utils.MetricNameUtils.replaceDots;
@@ -29,7 +30,12 @@ class App {
       PackageInfo packageInfo = context.getPackageManager().getPackageInfo(packageName, 0);
       return replaceDots(packageInfo.versionName);
     } catch (PackageManager.NameNotFoundException e) {
-      return context.getString(R.string.unknown_version_name_cross_metric_name);
+      String buildConfigVersionName = BuildConfig.VERSION_NAME;
+      if (buildConfigVersionName.isEmpty()) {
+        return context.getString(R.string.unknown_version_name_cross_metric_name);
+      } else {
+        return buildConfigVersionName;
+      }
     }
   }
 }
