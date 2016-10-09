c3f96e7b8ad492f53a9d9ce66b9b43d7e7a4ca61
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-10-09 17:57:29 +0200

    Implement report process in the sync service

--- flowup/src/main/java/com/flowup/reporter/android/WiFiSyncService.java
+++ flowup/src/main/java/com/flowup/reporter/android/WiFiSyncService.java
@@ -4,6 +4,11 @@
 
 package com.flowup.reporter.android;
 
+import com.flowup.R;
+import com.flowup.reporter.ReportResult;
+import com.flowup.reporter.apiclient.ApiClient;
+import com.flowup.reporter.model.Reports;
+import com.flowup.reporter.storage.ReportsStorage;
 import com.google.android.gms.gcm.GcmTaskService;
 import com.google.android.gms.gcm.TaskParams;
 
@@ -14,18 +19,28 @@ import static com.google.android.gms.gcm.GcmNetworkManager.RESULT_SUCCESS;
 
 public class WiFiSyncService extends GcmTaskService {
 
+  private ApiClient apiClient;
+  private ReportsStorage reportsStorage;
+
+  @Override public void onCreate() {
+    super.onCreate();
+    String scheme = getString(R.string.flowup_scheme);
+    String host = getString(R.string.flowup_host);
+    int port = getResources().getInteger(R.integer.flowup_port);
+    apiClient = new ApiClient(scheme, host, port);
+    reportsStorage = new ReportsStorage(this);
+  }
+
   @Override public int onRunTask(TaskParams taskParams) {
-    int result = RESULT_RESCHEDULE;
-    if (isTaskSupported(taskParams)) {
-      result = RESULT_FAILURE;
-    } else if (isWiFiConnectionEnabled()) {
-      if (synchronizeMetricsReport()) {
-        result = RESULT_SUCCESS;
-      } else {
-        result = RESULT_RESCHEDULE;
-      }
+    if (!isTaskSupported(taskParams)) {
+      return RESULT_FAILURE;
     }
-    return result;
+
+    if (!isWiFiConnectionEnabled()) {
+      return RESULT_RESCHEDULE;
+    }
+
+    return syncStoredReports();
   }
 
   private boolean isTaskSupported(TaskParams taskParams) {
@@ -36,7 +51,18 @@ public class WiFiSyncService extends GcmTaskService {
     return false;
   }
 
-  private boolean synchronizeMetricsReport() {
-    return false;
+  private int syncStoredReports() {
+    Reports reports = reportsStorage.getReports();
+    if (reports != null) {
+      ReportResult result = apiClient.sendReports(reports);
+      if (result.isSuccess()) {
+        reportsStorage.deleteReports(reports);
+        return RESULT_SUCCESS;
+      } else {
+        return RESULT_FAILURE;
+      }
+    } else {
+      return RESULT_SUCCESS;
+    }
   }
 }
