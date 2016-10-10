812da1caf251f3831ccf203f16c2f650ebd257e0
Pedro Vicente Gomez <pedro@gokarumi.com>
2016-10-10 13:32:20 +0200

    Fix some checkstyle violations

--- flowup/src/test/java/com/flowup/RobolectricTest.java
+++ flowup/src/test/java/com/flowup/RobolectricTest.java
@@ -5,13 +5,10 @@
 package com.flowup;
 
 import android.os.Build;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 @Config(constants = BuildConfig.class,
     sdk = Build.VERSION_CODES.LOLLIPOP,
-    packageName = "com.flowup")
-public class RobolectricTest {
+    packageName = "com.flowup") public class RobolectricTest {
 
 }
