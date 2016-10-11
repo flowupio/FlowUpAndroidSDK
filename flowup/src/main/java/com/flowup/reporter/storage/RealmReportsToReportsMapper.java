aa208853d18979ab1f5cc99e6a8a838fb1c5e219
Pedro Vicente Gómez Sánchez <pedrovgs@users.noreply.github.com>
2016-10-11 17:35:23 +0200

    Fix bug realted to different screens report not being reported (#66)
    * Disable console reporter
    
    * Change report unit to MILLISECONDS
    
    * Get a test showing the error
    
    * Bug fixed

--- flowup/src/main/java/com/flowup/reporter/storage/RealmReportsToReportsMapper.java
+++ flowup/src/main/java/com/flowup/reporter/storage/RealmReportsToReportsMapper.java
@@ -12,8 +12,10 @@ import com.flowup.reporter.model.UIMetric;
 import com.flowup.utils.Mapper;
 import com.flowup.utils.StatisticalValueUtils;
 import io.realm.RealmList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 class RealmReportsToReportsMapper extends Mapper<List<RealmReport>, Reports> {
 
@@ -109,33 +111,64 @@ class RealmReportsToReportsMapper extends Mapper<List<RealmReport>, Reports> {
 
   private List<UIMetric> mapUIMetricsReport(List<RealmReport> reports) {
     List<UIMetric> uiMetricsReports = new LinkedList<>();
-    StatisticalValue frameTime = null;
-    StatisticalValue framesPerSecond = null;
     for (int i = 0; i < reports.size(); i++) {
       RealmList<RealmMetric> metrics = reports.get(i).getMetrics();
-      for (int j = 0; j < metrics.size(); j++) {
-        RealmMetric metric = metrics.get(j);
-        String metricName = metric.getMetricName();
-        if (extractor.isFrameTimeMetric(metricName)) {
-          frameTime = StatisticalValueUtils.fromRealm(metric.getStatisticalValue());
-        } else if (extractor.isFPSMetric(metricName)) {
-          framesPerSecond = StatisticalValueUtils.fromRealm(metric.getStatisticalValue());
-        } else {
-          continue;
-        }
-        String versionName = extractor.getVersionName(metricName);
-        String osVersion = extractor.getOSVersion(metricName);
-        boolean batterySaverOne = extractor.getIsBatterSaverOn(metricName);
-        String screenName = extractor.getScreenName(metricName);
-        long timestamp = extractor.getTimestamp(metricName);
-        if (frameTime != null && framesPerSecond != null) {
-          uiMetricsReports.add(
-              new UIMetric(timestamp, versionName, osVersion, batterySaverOne, screenName,
-                  frameTime, framesPerSecond));
-          break;
-        }
-      }
+      List<UIMetric> uiMetricsPerReport = extractUIMetricsForReport(metrics);
+      uiMetricsReports.addAll(uiMetricsPerReport);
     }
     return uiMetricsReports;
+  }
+
+  private List<UIMetric> extractUIMetricsForReport(RealmList<RealmMetric> metrics) {
+    List<UIMetric> uiMetrics = new LinkedList<>();
+    Set<String> screenNames = extractScreenNames(metrics);
+    for (String screenName : screenNames) {
+      UIMetric uiMetric = extractUIMetric(screenName, metrics);
+      if (uiMetric != null) {
+        uiMetrics.add(uiMetric);
+      }
+    }
+    return uiMetrics;
+  }
+
+  private Set<String> extractScreenNames(RealmList<RealmMetric> metrics) {
+    Set<String> screenNames = new HashSet<>();
+    for (RealmMetric metric : metrics) {
+      String screenName = extractor.getScreenName(metric.getMetricName());
+      screenNames.add(screenName);
+    }
+    return screenNames;
+  }
+
+  private UIMetric extractUIMetric(String screenName, RealmList<RealmMetric> metrics) {
+    StatisticalValue frameTime = null;
+    StatisticalValue framesPerSecond = null;
+    for (int i = 0; i < metrics.size(); i++) {
+      RealmMetric metric = metrics.get(i);
+      String metricName = metric.getMetricName();
+      if (!extractor.isFPSMetric(metricName) && !extractor.isFrameTimeMetric(metricName)) {
+        continue;
+      }
+      String metricScreenName = extractor.getScreenName(metricName);
+      if (!metricScreenName.equals(screenName)) {
+        continue;
+      }
+      if (extractor.isFrameTimeMetric(metricName)) {
+        frameTime = StatisticalValueUtils.fromRealm(metric.getStatisticalValue());
+      } else if (extractor.isFPSMetric(metricName)) {
+        framesPerSecond = StatisticalValueUtils.fromRealm(metric.getStatisticalValue());
+      } else {
+        continue;
+      }
+      String versionName = extractor.getVersionName(metricName);
+      String osVersion = extractor.getOSVersion(metricName);
+      boolean batterySaverOne = extractor.getIsBatterSaverOn(metricName);
+      long timestamp = extractor.getTimestamp(metricName);
+      if (frameTime != null && framesPerSecond != null) {
+        return new UIMetric(timestamp, versionName, osVersion, batterySaverOne, screenName,
+            frameTime, framesPerSecond);
+      }
+    }
+    return null;
   }
 }
