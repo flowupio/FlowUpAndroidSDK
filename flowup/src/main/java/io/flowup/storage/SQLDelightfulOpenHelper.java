d076b262518975fb4483eea898884d2d681819a3
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2016-11-17 17:30:35 +0100

    Add more tables to the database

--- flowup/src/main/java/io/flowup/storage/SQLDelightfulOpenHelper.java
+++ flowup/src/main/java/io/flowup/storage/SQLDelightfulOpenHelper.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import io.flowup.config.storage.ConfigModel;
+import io.flowup.config.storage.MetricModel;
+import io.flowup.config.storage.ReportModel;
 
 public class SQLDelightfulOpenHelper extends SQLiteOpenHelper {
 
   private static final String DB_NAME = "flowup.db";
   private static final int DB_VERSION = 1;
+  private static final String REPORT_TIMESTAMP_INDEX =
+      "CREATE INDEX search_report_timestamp ON report (report_timestamp DESC)";
 
   public SQLDelightfulOpenHelper(Context context) {
     super(context, DB_NAME, null, DB_VERSION);
@@ -16,6 +20,9 @@ public class SQLDelightfulOpenHelper extends SQLiteOpenHelper {
 
   @Override public void onCreate(SQLiteDatabase db) {
     db.execSQL(ConfigModel.CREATE_TABLE);
+    db.execSQL(ReportModel.CREATE_TABLE);
+    db.execSQL(MetricModel.CREATE_TABLE);
+    db.execSQL(REPORT_TIMESTAMP_INDEX);
   }
 
   @Override public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
