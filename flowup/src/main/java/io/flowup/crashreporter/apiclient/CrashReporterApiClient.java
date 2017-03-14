b9a25e4ea942b1eb8d3c39d67ea07772358f879f
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-03-14 17:49:57 +0100

    Get the tests passing for our new crash reporter api client. Time to integrate it

--- flowup/src/main/java/io/flowup/crashreporter/apiclient/CrashReporterApiClient.java
+++ flowup/src/main/java/io/flowup/crashreporter/apiclient/CrashReporterApiClient.java
@@ -16,28 +16,32 @@ import okhttp3.Response;
 
 public class CrashReporterApiClient extends ApiClient {
 
-  private final HttpUrl reportCrashUrl;
+  private final HttpUrl reportErrorUrl;
 
   public CrashReporterApiClient(String apiKey, Device device, String scheme, String host, int port,
       boolean debugEnabled) {
-    super(apiKey, device, scheme, host, port, debugEnabled);
-    reportCrashUrl = baseUrl.newBuilder("/errorReport").build();
+    super(apiKey, device, scheme, host, port, debugEnabled, false);
+    reportErrorUrl = baseUrl.newBuilder("/errorReport").build();
   }
 
-  public ApiClientResult<Void> reportError(Throwable t) {
-    ErrorReport errorReport = mapExceptionToErrorReport(t);
-    RequestBody body = RequestBody.create(JSON, jsonParser.toJson(errorReport));
-    Request request = new Request.Builder().url(reportCrashUrl).post(body).build();
+  public ApiClientResult<Object> reportError(Throwable t) {
+    Request request = generateCreateErrorReportRequest(t);
     try {
       Response response = httpClient.newCall(request).execute();
       if (response.isSuccessful()) {
         response.close();
-        return new ApiClientResult<>((Void) null);
+        return new ApiClientResult<>(new Object());
       }
     } catch (IOException e) {
       return new ApiClientResult<>(ApiClientResult.Error.NETWORK_ERROR);
     }
     return new ApiClientResult<>(ApiClientResult.Error.UNKNOWN);
+  }
+
+  private Request generateCreateErrorReportRequest(Throwable t) {
+    ErrorReport errorReport = mapExceptionToErrorReport(t);
+    RequestBody body = RequestBody.create(JSON, jsonParser.toJson(errorReport));
+    return new Request.Builder().url(reportErrorUrl).post(body).build();
   }
 
   private ErrorReport mapExceptionToErrorReport(Throwable t) {
